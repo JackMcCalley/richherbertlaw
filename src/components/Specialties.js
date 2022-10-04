d9e0a52006fcabb471ecc1914666f4ca6ba608c1
Jack McCalley <34116924+JackMcCalley@users.noreply.github.com>
2022-10-04 14:35:30 -0700

    changed up cards

--- src/components/Specialties.js
+++ src/components/Specialties.js
@@ -1,5 +1,6 @@
-import React, {forwardRef} from "react";
+import React, { forwardRef } from "react";
 import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
+import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
@@ -7,12 +8,13 @@ import Typography from "@mui/material/Typography";
 import { CardActionArea } from "@mui/material";
 import housing from "../assets/housing.jpg";
 import notebook from '../assets/notebook.jpg'
+import '../styles/Specialties.css'
 
 const Specialties = forwardRef((props, ref) => {
   const Specialty = (props) => {
     return (
       <Grid2 item xs={12} align='center' marginBottom='2rem'>
-        <Card raised sx={{ maxWidth: "70%" }}>
+        <Card raised sx={{ maxWidth: "100%" }}>
           <CardActionArea>
             <div style={{ position: 'relative' }}>
               <CardMedia
@@ -26,29 +28,40 @@ const Specialties = forwardRef((props, ref) => {
               </div>
             </div>
             <CardContent>
-              <Typography>
+              <Typography sx={{fontSize: '1.5rem'}}>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
               </Typography>
             </CardContent>
-          </CardActionArea>
-        </Card>
-      </Grid2>
+        </CardActionArea>
+      </Card>
+
+      </Grid2 >
     );
   };
+
+const Specialty2 = (props) => {
   return (
-    <section ref={ref}>
+    <Box sx={{ width: '100vw', margin: 0, backgroundColor: 'black' }}>
+      Specialties
+    </Box>
+  )
+}
+return (
+  <section ref={ref}>
     <Grid2
       container
       direction="column"
       alignItems="center"
       justifyContent="center"
     >
-      <h1 style={styles.title}>Specialties</h1>
+      <Box sx={{ width: '100%', margin: '0px' }}>
+        <h1 className="specialty-title">SPECIALTIES</h1>
+      </Box>
       <Specialty image={housing} title="HOUSING LAW" description="" />
       <Specialty image={notebook} title="Criminal Defense" description="" />
     </Grid2>
-    </section>
-  );
+  </section>
+);
 })
 
 const styles = {
@@ -57,10 +70,10 @@ const styles = {
     display: "flex",
     justifyContent: "center",
     flexDirection: 'column',
-    height: 'auto'
+    height: 'auto',
   },
   title: {
-    fontSize: '36px',
+    fontSize: '68px',
 
   }
 };
